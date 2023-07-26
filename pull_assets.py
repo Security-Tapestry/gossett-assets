b2426ddf759aaaabae010fa3cc3103a48bc0aa04
Natalya Higdon <nhigdon@securitytapestry.com>
2023-07-26 16:12:30 -0500

    new file:   assets.json 	new file:   pull_assets.py

---
+++ pull_assets.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def pull_assets():
+    # asset_filter = "department_id:21000185204"
+    url = "https://securitytapestry.freshservice.com/api/v2/assets?filter=\"department_id:21000185204\""
+    request = requests.get(url, auth=('p4CWhwgKyWmyrJrKWJ','X'))
+    response = request.json()["assets"]
+    print(response)
+    with open("assets.json","w",encoding="UTF-8") as assets:
+        json.dump(response, assets, indent=4)
+
+if __name__ == "__main__":
+    pull_assets()
