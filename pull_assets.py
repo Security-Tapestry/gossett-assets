ce96ff7bb0bf7f4a42edefa0ffca92854105d1c7
Natalya Higdon <nhigdon@securitytapestry.com>
2023-07-26 17:07:12 -0500

    modified:   pull_assets.py

--- pull_assets.py
+++ pull_assets.py
@@ -5,7 +5,7 @@ def pull_assets():
     # asset_filter = "department_id:21000185204"
     url = "https://securitytapestry.freshservice.com/api/v2/assets?filter=\"department_id:21000185204\""
     request = requests.get(url, auth=('p4CWhwgKyWmyrJrKWJ','X'))
-    response = request.json()
+    response = request.json()["assets"]
     print(response)
     with open("docs/assets.json","w",encoding="UTF-8") as assets:
         json.dump(response, assets, indent=4)
