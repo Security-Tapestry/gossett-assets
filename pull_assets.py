e10a076c3b1758f9a58670d3f7f0a06e1b138dc1
Natalya Higdon <nhigdon@securitytapestry.com>
2023-07-26 17:35:43 -0500

    modified:   pull_assets.py

--- pull_assets.py
+++ pull_assets.py
@@ -2,13 +2,24 @@ import requests
 import json
 
 def pull_assets():
-    # asset_filter = "department_id:21000185204"
     url = "https://securitytapestry.freshservice.com/api/v2/assets?filter=\"department_id:21000185204\""
     request = requests.get(url, auth=('p4CWhwgKyWmyrJrKWJ','X'))
     response = request.json()["assets"]
-    print(response)
-    with open("docs/assets.json","w",encoding="UTF-8") as assets:
-        json.dump(response, assets, indent=4)
+    for asset in response:
+        get_asset_details(asset)
+    # print(response)
+    # save_asset_json(response)
+
+def get_asset_details(asset):
+    url = "https://securitytapestry.freshservice.com/api/v2/assets/" + str(asset["display_id"]) + "?include=type_fields"
+    request = requests.get(url, auth=('p4CWhwgKyWmyrJrKWJ','X'))
+    response = request.json()["asset"]
+    save_asset_json(response)
+
+def save_asset_json(data):
+    display_id = str(data["display_id"])
+    with open(f"docs/assets/asset-{display_id}.json","w",encoding="UTF-8") as asset:
+        json.dump(data, asset, indent=4)
 
 if __name__ == "__main__":
     pull_assets()
