e19361ba0f0f42b2ccc2172c36dbb77f763bac65
Natalya Higdon <queenlinuxtechgroup@gmail.com>
2023-07-27 14:38:48 -0500

    rename json keys

--- pull_assets.py
+++ pull_assets.py
@@ -51,7 +51,7 @@ def clean_json(json_input):
         'agent_id','group_id','assigned_on',
         'created_at','updated_at','end_of_life',
         'discovery_enabled','author_type','asset_type_id',
-        'department_id'
+        'department_id','display_id','id'
         ]
     remove_keys_secondary = [
         'vendor_21001393125',
@@ -59,9 +59,27 @@ def clean_json(json_input):
         'warranty_21001393125',
         'acquisition_date_21001393125',
         'warranty_expiry_date_21001393125',
-        'depreciation_id',
-        'salvage'
+        'depreciation_id','salvage',
+        'product_21001393125'
     ]
+    rename_keys = {
+        'domain_21001393125': 'Domain',
+        'asset_state_21001393125': 'State',
+        'serial_number_21001393125': 'Serial_Number',
+        'os_21001393130': 'OS',
+        'os_version_21001393130': 'Version',
+        'os_service_pack_21001393130': 'Service_Pack',
+        'memory_21001393130': 'Memory_GB',
+        'disk_space_21001393130': 'Disk_Space_GB',
+        'cpu_speed_21001393130': 'CPU_Speed_GHz',
+        'cpu_core_count_21001393130': 'CPU_Core_Count',
+        'mac_address_21001393130': 'MAC_Address',
+        'uuid_21001393130': 'UUID',
+        'hostname_21001393130': 'Hostname',
+        'computer_ip_address_21001393130': 'IP_Address',
+        'last_login_by_21001393130': 'Last_Logged_in_User',
+        'last_audit_date_21001393125': 'Last_Check_In'
+    }
     iterator = 0
     for asset in json_input:
         if asset['author_type'] != 'Discovery Agent':
@@ -72,6 +90,8 @@ def clean_json(json_input):
             del asset[key]
         for key in remove_keys_secondary:
             del asset['type_fields'][key]
+        for key,value in rename_keys.items():
+            asset['type_fields'][value] = asset['type_fields'].pop(key)
 
     return json_input
 
