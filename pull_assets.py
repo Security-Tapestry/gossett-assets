b889ef3ca02f43daf3f5d59861fd3893198d630e
Natalya Higdon <nhigdon@securitytapestry.com>
2023-07-27 10:47:18 -0500

    modified:   pull_assets.py

--- pull_assets.py
+++ pull_assets.py
@@ -6,6 +6,7 @@
 import os
 import json
 from dotenv import load_dotenv
+from asposecells.api import Workbook
 import requests
 
 if not os.getenv("FS_API"):
@@ -14,15 +15,21 @@ if not os.getenv("FS_API"):
 
 def pull_assets():
     url = 'https://securitytapestry.freshservice.com/api/v2/assets?filter="department_id:21000185204"&include=type_fields'
-    request = requests.get(url, auth=(os.getenv("FS_API"), "X"), timeout=30)
-    response = request.json()["assets"]
+    request = requests.get(url, auth=(os.getenv('FS_API'), 'X'), timeout=30)
+    response = request.json()['assets']
     save_asset_json(response)
+    create_html('docs/assets.json')
 
 
 def save_asset_json(data):
-    with open("docs/assets.json", "w", encoding="UTF-8") as assets:
+    with open('docs/assets.json', 'w', encoding='UTF-8') as assets:
         json.dump(data, assets, indent=4)
 
 
-if __name__ == "__main__":
+def create_html(json):
+    workbook = Workbook(json)
+    workbook.save('docs/index.html')
+
+
+if __name__ == '__main__':
     pull_assets()
