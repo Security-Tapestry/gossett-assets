0229c5b00625abec6cb16db17ce42f93c597830c
Natalya Higdon <queenlinuxtechgroup@gmail.com>
2023-07-27 10:31:14 -0500

    clean

--- pull_assets.py
+++ pull_assets.py
@@ -1,20 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Module for pulling assets and creating views"""
+
+import os
+import json
 from dotenv import load_dotenv
 import requests
-import json
-import os
 
 if not os.getenv("FS_API"):
     load_dotenv()
 
+
 def pull_assets():
-    url = "https://securitytapestry.freshservice.com/api/v2/assets?filter=\"department_id:21000185204\"&include=type_fields"
-    request = requests.get(url, auth=(os.getenv("FS_API"),'X'))
+    url = 'https://securitytapestry.freshservice.com/api/v2/assets?filter="department_id:21000185204"&include=type_fields'
+    request = requests.get(url, auth=(os.getenv("FS_API"), "X"), timeout=30)
     response = request.json()["assets"]
     save_asset_json(response)
 
+
 def save_asset_json(data):
-    with open("docs/assets.json","w",encoding="UTF-8") as assets:
+    with open("docs/assets.json", "w", encoding="UTF-8") as assets:
         json.dump(data, assets, indent=4)
+
 
 if __name__ == "__main__":
     pull_assets()
