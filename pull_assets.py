ad5e0faa8f90e1f60a85760880d2a90204204a2b
Natalya Higdon <queenlinuxtechgroup@gmail.com>
2023-08-08 14:24:27 -0500

    fix

--- pull_assets.py
+++ pull_assets.py
@@ -196,13 +196,13 @@ def create_html(json_input):
             + '<script src="assets/key.js"></script>\n'
             + f'<a id="assetCount">Assets: {len(json_input)}</a><br><br>\n'
             + '<input type="text" id="hostnameSearch" onkeyup="filterHostname()" placeholder="Hostname...">\n' # pylint: disable=C0301
-            + '&ensp;'
+            + '&nbsp;'
             + '<input type="text" id="serialSearch" onkeyup="filterSerial()" placeholder="Serial/Asset Tag...">\n' # pylint: disable=C0301
-            + '&ensp;'
+            + '&nbsp;'
             + '<input type="text" id="macSearch" onkeyup="filterMAC()" placeholder="MAC Address...">\n' # pylint: disable=C0301
-            + '&ensp;'
+            + '&nbsp;'
             + '<input type="text" id="ipSearch" onkeyup="filterIP()" placeholder="IP Address...">\n' # pylint: disable=C0301
-            + '&ensp;'
+            + '&nbsp;'
             + '<input type="text" id="productSearch" onkeyup="filterProduct()" placeholder="Product...">\n' # pylint: disable=C0301
             + html_string
             + '\n</body>\n<script src="assets/filtertable.js"></script>\n'
